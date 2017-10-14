b82fe39aa3e3270436a8222923e600d00b941623
dudesez <aaronseibring@gmail.com>
2017-10-14 00:00:50 -0400

    Messed with nav a bit. Need to commit to pull

--- nav.php
+++ nav.php
@@ -2,9 +2,13 @@
 
 <h1 class="white" id="name">Fish Vermont</h1>
     <p class="white"><STRONG>Find some Fish.</STRONG></p>
-    
-    
-<nav>
+
+
+<nav id='nav'>
+  <a href="index_copy.php"><img src="/images/fish.png" alt="FishyBoi" height="100"></a>
+    <article class="welcome">
+    <p class="tagLine">Let's get Fishy</p>
+    </article>
     <ol>
         <?php
         print '<li class = "';
@@ -14,7 +18,7 @@
         print '">';
         print '<a href="index.php">Home</a>';
         print '</li>';
-        
+
         print '<li class="';
         if ($path_parts['filename'] == "map") {
             print ' activePage ';
