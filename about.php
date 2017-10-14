36c3efdd9cf86987e9e89fd800451696ba7d97aa
n0lley <ntolley13@gmail.com>
2017-10-14 00:33:05 -0400

    About page written. Need to proofread when I've gotten sleep and stop misspelling every other word.

---
+++ about.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<HTML lang="en">
+
+    <head>
+        <title>About Fish VT</title>
+        <meta charset="utf-8">
+        <meta name="author" content="UVM CS Crew">
+        <meta name="description" content="HackVT">
+        <link rel="icon" type="image/png" href="images/Pufferfish.png">
+        <link rel="stylesheet" href="final.css" type="text/css" media="screen">
+    </head>
+
+    <body>   
+    <?php include ("nav.php"); ?>
+    <br><br>
+    
+    <!--Get and display user location -->
+    
+    <div id="rectangle">
+        
+        <p>
+            The goal of FishVT is to raise awareness of the fantastic fishing opportunities that Vermont has to offer.
+            In Vermont, fishing is mostly overshadowed by the state's more popular sports, such as skiing or hiking. 
+            But Vermont is home to over 90 species of fish, from common game fish like bass and trout to more exotic
+            catches like the Longnose Gar or the Bowfin. In order to let visitors and residents alike know about the
+            options available to them, FishVT has created an accessible, easy-to-use, and highly customizable web site 
+            where fishing enthusiasts can locate the best fishing holes nearby.
+        </p>
+        
+   </div>
+</body>
+</html>
