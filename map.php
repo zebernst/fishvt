0b2e7a14e2e0a2b4762b988499b324759b7a3b5e
dudesez <aaronseibring@gmail.com>
2017-10-14 04:08:52 -0400

    Tried to fix garbage code git put in my commit

--- map.php
+++ map.php
@@ -83,53 +83,47 @@ $parkingChoice = array("chkSmall", "chkMedium", "chkLarge");
             ?>
                 </table>
             </fieldset>
-<<<<<<< HEAD
 
-
-=======
-            
             <fieldset class="checkbox contact">
                 <legend>Traffic: Check all that apply</legend>
                 <table style = "width: 100%">
-                <?php 
-                for($x = 0; $x < count($trafficList); $x++) {  
+                <?php
+                for($x = 0; $x < count($trafficList); $x++) {
                     if($x == 0) { echo "<tr>";}
                     echo "<th>";
                     echo "<label><input
                             id='" . $trafficChoice[$x] . "'
-                            name='" . $trafficChoice[$x] . "' 
+                            name='" . $trafficChoice[$x] . "'
                             type='checkbox'
                             value='" . $trafficList[$x] . "'>" . $trafficList[$x] . "</label> ";
                     echo "</th>";
-            } 
+            }
             echo "</tr>";
-            
+
             ?>
                 </table>
             </fieldset>
-            
+
             <fieldset class="checkbox contact">
                 <legend>Parking: Check all that apply</legend>
                 <table style = "width: 100%">
-                <?php 
-                for($x = 0; $x < count($parkingList); $x++) {  
+                <?php
+                for($x = 0; $x < count($parkingList); $x++) {
                     if($x == 0) { echo "<tr>";}
                     echo "<th>";
                     echo "<label><input
                             id='" . $parkingChoice[$x] . "'
-                            name='" . $parkingChoice[$x] . "' 
+                            name='" . $parkingChoice[$x] . "'
                             type='checkbox'
                             value='" . $parkingList[$x] . "'>" . $parkingList[$x] . "</label> ";
                     echo "</th>";
-            } 
+            }
             echo "</tr>";
-            
+
             ?>
                 </table>
             </fieldset>
-            
-            
->>>>>>> 9a6107241d62056a7d18ed30522761e7586fc9c0
+
             <p id="demo"></p>
             <?php include ("geolocation.php");?>
 
@@ -139,15 +133,8 @@ $parkingChoice = array("chkSmall", "chkMedium", "chkLarge");
                 <input class="button" onclick="getLocation()" id="btnSubmit" name="btnSubmit" tabindex="900" type="submit" value="Register" >
             </fieldset> <!--Ends Buttons-->
         </form>
-<<<<<<< HEAD
 
         <p>You selected: <?php
-
-=======
-        
-        <p>You selected: <br> <?php 
-        
->>>>>>> 9a6107241d62056a7d18ed30522761e7586fc9c0
         for($x = 0; $x <= count($fishChoice); $x++) {
         echo $_POST[$fishChoice[$x]];
         echo " ";
@@ -159,25 +146,20 @@ $parkingChoice = array("chkSmall", "chkMedium", "chkLarge");
         echo $_POST[$binaryChoice[$x]];
         echo " ";
         }
-<<<<<<< HEAD
+        echo "<br>";
 
-=======
-        
-        echo "<br>";
-        
         for($x = 0; $x <= count($trafficChoice); $x++) {
-        echo $_POST[$trafficChoice[$x]]; 
+        echo $_POST[$trafficChoice[$x]];
         echo " ";
         }
-        
+
         echo "<br>";
-        
+
         for($x = 0; $x <= count($parkingChoice); $x++) {
-        echo $_POST[$parkingChoice[$x]]; 
+        echo $_POST[$parkingChoice[$x]];
         echo " ";
         }
-        
->>>>>>> 9a6107241d62056a7d18ed30522761e7586fc9c0
+
         ?>
 
         </p>
