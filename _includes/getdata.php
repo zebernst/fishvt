c962b65c09e12a0cc4e8562d30e8f325b701caaa
Zach Bernstein <zacharybernstein42@me.com>
2017-10-13 23:20:35 -0400

    renamed and relocated files

--- _includes/getdata.php
+++ _includes/getdata.php
@@ -5,15 +5,14 @@ if (isset($_GET['debug'])) {
 	$debug = true;
 }
 
-
-
 // fetch data
-$output = shell_exec(escapeshellcmd('python ./getdata.py 44.518087699999995 -73.18415689999999'));
+$output = shell_exec(escapeshellcmd('python ../_scripts/getdata.py 44.518087699999995 -73.18415689999999'));
 $data = json_decode($output);
 
 // print data array
 if ($debug) {
 	print "<pre>";
+	print '$data array:'."\n";
 	print_r($data);
 	print "</pre>";
 }
