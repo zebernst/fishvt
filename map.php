655ba6136b167a4b0acaf820ceb1b26506f3ef10
pjsolomon <pauljack.solomon@gmail.com>
2017-10-14 03:07:09 -0400

    Update to Map (Tables)

--- map.php
+++ map.php
@@ -1,9 +1,9 @@
 <!DOCTYPE HTML>
 
-<?php
+<?php 
 $fishList = array("Bowfin", "Carp", "ChannelCatfish", "WhiteCrappie", "LongnoseGar", "Muskellunge", "WhitePerch", "AmericanShad", "Sheepshead",
-        "LakeWhitefish", "BrookTrout", "BrownTrout", "RainbowTrout", "LakeTrout", "LandlockedSalmon", "RainbowSmelt",
-        "YellowPerch", "Walleye", "NorthernPike", "ChainPickeral", "LargemouthBass", "SmallmouthBass", "Bullhead",
+        "LakeWhitefish", "BrookTrout", "BrownTrout", "RainbowTrout", "LakeTrout", "LandlockedSalmon", "RainbowSmelt", 
+        "YellowPerch", "Walleye", "NorthernPike", "ChainPickeral", "LargemouthBass", "SmallmouthBass", "Bullhead", 
         "Panfish", "BlackCrappie", "Burbot");
 
 $fishChoice = array("chkBowfin", "chkCarp", "chkChannelCatfish", "chkWhiteCrappie", "chkLongnoseGar", "chkMuskellunge", "chkWhitePerch",
@@ -23,53 +23,63 @@ $fishChoice = array("chkBowfin", "chkCarp", "chkChannelCatfish", "chkWhiteCrappi
         <link rel="stylesheet" href="final.css" type="text/css" media="screen">
     </head>
 
-    <body>
+    <body>   
     <?php include ("nav.php"); ?>
     <br><br>
-
+    
     <!--Get and display user location -->
-
+    
     <div id="rectangle">
-
+        
+    
         <!-- PROMPT USER FOR SOME FISH -->
-        <form action="<?php print $phpSelf; ?>" id="frmRegister" method = "post">
+        <form action="<?php print $phpSelf; ?>" id="frmRegister" method = "post"> 
             <fieldset class="checkbox contact">
                 <legend>Fish: Check all that apply</legend>
-
-                <?php
-                for($x = 0; $x < count($fishList); $x++) {
-
+                <table style = "width: 100%">
+                <?php 
+                for($x = 0; $x < count($fishList); $x++) {  
+                    if($x % 5 == 0) { echo "<tr>";}
+                    echo "<th>";
                     echo "<label><input
                             id='" . $fishChoice[$x] . "'
-                            name='" . $fishChoice[$x] . "'
+                            name='" . $fishChoice[$x] . "' 
                             type='checkbox'
                             value='" . $fishList[$x] . "'>" . $fishList[$x] . "</label> ";
-            }
-
+                    echo "</th>";
+                    if($x % 5 == 5) { echo "</tr>";}
+            } 
+            
             ?>
-
+                </table>
             </fieldset>
-
+            
+            
+            <p id="demo"></p>
+            <?php include ("geolocation.php");?>
+            
             <!--BUTTONS AND WIRES-->
              <fieldset class ="buttons">
                 <legend></legend>
-                <input class="button" id="btnSubmit" name="btnSubmit" tabindex="900" type="submit" value="Register" >
+                <input class="button" onclick="getLocation()" id="btnSubmit" name="btnSubmit" tabindex="900" type="submit" value="Register" >
             </fieldset> <!-- Ends Buttons -->
         </form>
-
-        <p>You selected: <?php
-
+        
+        <p>You selected: <?php 
+        
         for($x = 0; $x <= count($fishChoice); $x++) {
-        echo $_POST[$fishChoice[$x]];
+        echo $_POST[$fishChoice[$x]]; 
         echo " ";
         }
-
+        
         ?>
-
+        
         </p>
-
-
-
+        
+        
+        
+        
+        
    </div>
 </body>
 </html>
